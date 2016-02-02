2837e85c281feaeba09233569f6a10bcbfe0428c
Lei SHI <lei.shi@wuneo.com>
2016-02-02 02:53:35 -0500

    Re-done gulp task - `clean` .

--- gulpfile/env/clean.js
+++ gulpfile/env/clean.js
@@ -1,12 +1,42 @@
-'use strict';
+/***********************************************************
+ *  \#\#\# Gulp task for Cleaning \#\#\#
+ *
+ *
+ ***********************************************************/
 
-var gulp = require('gulp');
-var del = require('del');
+(function () {
+    "use strict";
 
-var config = require('../config');
-var cli = require('../cli');
+    var gulp                    = require('gulp');
+    var gutil                   = require('gulp-util');
+    var fs                      = require('fs');
+    var path                    = require('path');
 
-// clean target dir
-gulp.task('clean', function(done) {
-    del([cli.targetDir], done);
-});
+    var del                     = require('del');
+
+    var config                  = require('../config');
+    var cli                     = require('../cli');
+    var logger                  = require('../util/logger');
+    var handleErr               = require('../util/handleErr');
+    var pathBuilder             = require('../util/pathBuilder');
+
+    // Gulp task name
+    var taskName = "clean";
+
+    //----------------------------------------------------------
+    // Gulp Tasks
+    //----------------------------------------------------------
+
+    gulp.task(taskName, function() {
+        del([
+            cli.solveTargetFolderPath()
+        ]);
+        logger.info(taskName, "Clean Paths:", [cli.solveTargetFolderPath()])
+    });
+
+    //----------------------------------------------------------
+    // Internal Functions
+    //----------------------------------------------------------
+
+}());
+
