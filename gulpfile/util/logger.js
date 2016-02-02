d26211c1042cb526cfeb6d1c15d7f1877291b577
Lei SHI <lei.shi@wuneo.com>
2016-02-02 02:25:14 -0500

    Add new **Util** - `logger`
    - It utilize `gulp util` and provide a better way to do **coloring** when doing log

---
+++ gulpfile/util/logger.js
@@ -0,0 +1,89 @@
+/***********************************************************
+ *  \#\#\# Gulp Helper - Logging \#\#\#
+ *
+ *
+ ***********************************************************/
+
+(function () {
+    "use strict";
+
+    var gulp                    = require('gulp');
+    var gutil                   = require('gulp-util');
+
+    var msgPrefix = '----| Gulp Flow |==[-';
+
+    var exports = {
+        info: info,
+        warn: warn,
+        error: error
+    };
+
+    module.exports = exports;
+
+    //----------------------------------------------------------
+    // Interface Functions
+    //----------------------------------------------------------
+
+    function info() {
+        if (arguments.length < 2) {
+            return;
+        }
+
+        var taskName = arguments[0];
+        var msgArgs = new Array(arguments.length -1);
+        var msg = '';
+        for(var i = 0; i < msgArgs.length; ++i) {
+            //i is always valid index in the arguments object
+            msgArgs[i] = arguments[i+1];
+            msg += msgArgs[i].toString() + ' ';
+        }
+        var output = _buildMsg(msgPrefix, taskName, msg);
+        gutil.log(gutil.colors.green(output));
+    }
+
+    function warn() {
+        if (arguments.length < 2) {
+            return;
+        }
+
+        var taskName = arguments[0];
+        var msgArgs = new Array(arguments.length -1);
+
+        var msg = '';
+        for(var i = 0; i < msgArgs.length; ++i) {
+            //i is always valid index in the arguments object
+            msgArgs[i] = arguments[i+1];
+            msg += msgArgs[i].toString() + ' ';
+        }
+        var output = _buildMsg(msgPrefix, taskName, msg);
+        gutil.log(gutil.colors.purple(output));
+    }
+
+    function error() {
+        if (arguments.length < 2) {
+            return;
+        }
+
+        var taskName = arguments[0];
+        var msgArgs = new Array(arguments.length -1);
+
+        var msg = '';
+        for(var i = 0; i < msgArgs.length; ++i) {
+            //i is always valid index in the arguments object
+            msgArgs[i] = arguments[i+1];
+            msg += msgArgs[i].toString() + ' ';
+        }
+        var output = _buildMsg(msgPrefix, taskName, msg);
+        gutil.log(gutil.colors.red(output));
+    }
+
+
+    //----------------------------------------------------------
+    // Internal Functions
+    //----------------------------------------------------------
+
+    function _buildMsg(prefix, taskName, msg) {
+        return prefix + taskName +'-]===> '+ msg;
+    }
+
+}());
