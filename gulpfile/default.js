7512358b4bd516320a33d46369cca97eff9765f7
Lei SHI <lei.shi@wuneo.com>
2016-02-03 03:14:18 -0500

    Re-done `default`.
    Now, **Gulp Flow** closes its main loop - the entire **dev** env. is done the setting.

--- gulpfile/default.js
+++ gulpfile/default.js
@@ -1,25 +1,66 @@
-/* ***************************************************************************
- * ### Gulp - default task ###
+/***********************************************************
+ *  \#\#\# Gulp task for Default Task \#\#\#
  *
- */
+ *
+ ***********************************************************/
 
-'use strict';
+(function () {
+    "use strict";
 
-var gulp        = require('gulp');
-var gulpSequence    = require('gulp-sequence');
+    var gulp                    = require('gulp');
+    var gutil                   = require('gulp-util');
+    var fs                      = require('fs');
+    var path                    = require('path');
+
+    var runSequence             = require('run-sequence');
+
+    var config                  = require('./config');
+    var cli                     = require('./cli');
+    var handleErr               = require('./util/handleErr');
+    var pathBuilder             = require('./util/pathBuilder');
 
 
-gulp.task('default', function(cb){
 
-    gulpSequence('clean', 'browserify', cb);
-});
+    // Gulp task name
+    var taskName = "default";
+
+    //----------------------------------------------------------
+    // Gulp Tasks
+    //----------------------------------------------------------
+
+    // no-op = empty function
+    gulp.task('noop', function() {});
+
+    gulp.task(taskName, function(done) {
+        runSequence(
+            'clean',
+            'vendor',
+            'iconfont',
+            'templates',
+            [
+                'fonts',
+                'appicon',
+                'images',
+                'main-styles',
+                'main-scripts'
+            ],
+            'index',
+            cli.requiredBuild ? 'noop' : 'serve',
+            cli.requiredBuild ? 'noop' : 'watch',
+            //emulate ? ['ionic:emulate', 'watchers'] : 'noop',
+            //run ? 'ionic:run' : 'noop',
+            done);
+    });
 
 
-// Ensure process ends after all Gulp tasks are finished
-gulp.on('stop', function () {
-    if ( !global.isWatching ) {
-        process.nextTick(function () {
-            process.exit(0);
-        });
-    }
-});
+
+    //// Ensure process ends after all Gulp tasks are finished
+    //gulp.on('stop', function () {
+    //    if ( !global.isWatching ) {
+    //        process.nextTick(function () {
+    //            process.exit(0);
+    //        });
+    //    }
+    //});
+
+}());
