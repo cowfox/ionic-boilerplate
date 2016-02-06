dc900e5d2f6da09ffcfb16105c28a27bc1a7470e
Lei SHI <lei.shi@wuneo.com>
2016-02-06 03:10:14 -0500

    Update `handleErr` task - trying to make `gulp-notify` working.

--- gulpfile/util/handleErr.js
+++ gulpfile/util/handleErr.js
@@ -7,9 +7,9 @@
 (function () {
     "use strict";
 
+    var gulp        = require( 'gulp' )
+    var gutil       = require('gulp-util')
     var notify      = require('gulp-notify');
-
-    var beep        = require('beepbeep');
 
     var cli         = require('../cli');
     var logger      = require('./logger');
@@ -19,21 +19,23 @@
      */
     module.exports = function(error) {
 
-        logger.error('', error.code, error.message);
+        logger.error('errorHandler', error.toString());
 
         // If it is in "release" mode, end the whole process
         // after certain error.
         if (!cli.inReleaseMode) {
-            // Do "beep" sound
-            beep(3, 400);
 
-            var args = new Array(arguments.length);
+            var lineNumber = (error.lineNumber) ? 'LINE ' + error.lineNumber + ' -- ' : '';
 
             // Send error to notification center with gulp-notify
             notify.onError({
-                title: '[Gulp Flow] "Release" Compile Error',
-                message: '<%= error.message %>'
-            }).apply(this, args);
+                title: '[Gulp Flow] Error: [' + error.plugin + '] ' + lineNumber,
+                message:  '<%= error.message %>'
+            });
+            // Seems that it need `write()` to work, but not sure which pacakge to load.
+            //}).write(error);
+
+            gutil.beep();
 
             // Keep gulp from hanging on this task
             this.emit('end');
